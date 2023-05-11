0f207847cddbe84a4c55a76c8f08ecd819316cde
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2023-05-10 21:05:42 -0400

    5/10

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import path from "path";
+import dotenv from "dotenv";
 
 // routes
 import defaultRouter from "./routes/default";
@@ -10,10 +11,15 @@ const app = express();
 const port =
   process.env.PORT || process.env.NODE_ENV === "production" ? 80 : 5000;
 
+// load environment variables from .env file
+dotenv.config();
+
 // view engine setup, use ejs templates in the views folder
-// // use res.render from within an express controller to load up an ejs view file
+// use res.render from within an express controller to load up an ejs view file
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// set the public folder to serve public assets
 app.use(express.static(path.join(__dirname, "public")));
 
 // configure express app to use express.json middleware
@@ -23,7 +29,7 @@ app.use(express.json());
 // cookie parser
 app.use(cookieParser());
 
-// // routes
+// routes
 app.use("/", defaultRouter);
 app.use("/api", exampleApiRouter);
 
